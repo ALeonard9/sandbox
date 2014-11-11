95a3e1575a782ddd8eec83307ea795935bdd60d8
ALeonard9 <aleonard9@hotmail.com>
2014-11-10 20:15:37 -0500

    Encrypted try #1

--- recipes/default.rb
+++ recipes/default.rb
@@ -10,7 +10,7 @@
 include_recipe "apache2"
 include_recipe "apache2::mod_php5"
 
-# Unencrypted data bag option
+# #Unencrypted data bag option
 # data_bag("BettingBag").each do |item|
 # 	item_data = data_bag_item("BettingBag", item)
 
@@ -26,16 +26,16 @@ include_recipe "apache2::mod_php5"
 # end
 
 # Encrypted data bag option
-item_data = Chef::EncryptedDataBagItem.load("projectorion", "projectorion")
+item_data = Chef::EncryptedDataBagItem.load("projectorion-bag", "projectorion")
 
 template "/var/www/cgi-bin/connectToDB.php" do
-  source "connectToDB.php.erb"
-  mode "0644"
-   variables(
-     :connect => item_data["connection_string"],
-     :user => item_data["user"],
-     :userid => item_data["userid"]
-   )
+ source "connectToDB.php.erb"
+ mode "0644"
+ variables(
+   :connection => item_data["connection_string"],
+   :user => item_data["user"],
+   :password => item_data["userid"]
+ )
 end
 
 
