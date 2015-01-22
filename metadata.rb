ef16788fb82c378c28b73461db9cd1cc2180c1b6
ALeonard9 <aleonard9@hotmail.com>
2015-01-21 23:18:14 -0500

    New

--- metadata.rb
+++ metadata.rb
@@ -5,6 +5,6 @@ license          'All rights reserved'
 description      'Installs/Configures Betting'
 long_description 'Installs/Configures Betting'
 
-version          '1.0.4'
+version          '1.0.5'
 
-depends 'apache2'
+depends 'apache2', '2.0.0'
