e0dccd8526e0a791fcdb1dacefcde349ab467793
Adam Leonard <LeoNineStudios@outlook.com>
2015-05-19 01:22:28 -0400

    Removed keys

--- files/default/app/pivot.php
+++ files/default/app/pivot.php
@@ -15,10 +15,10 @@ if ($_SESSION['username'])
         {
         if ($_SESSION['usergroup']=="Admin")
                 {
-                echo "<br><button class='compact' onclick=location.href='betting.php'>Betting Game</button>";
+                echo "<br><button class='compact' onclick=location.href='bet/betting.php'>Betting Game</button>";
                 echo "<br><button class='compact' onclick=location.href='bet/betting.php'>TV</button>";
                 echo "<br><button class='compact' onclick=location.href='videogame/videogame.php'>Video Games</button>";
-                echo "<br><button class='compact' onclick=location.href='betting.php'>Movies</button>";
+                echo "<br><button class='compact' onclick=location.href='bet/betting.php'>Movies</button>";
                 // echo "<br><table style='border-spacing: 0px; padding: 0px; border-collapse:separate;'><tr><td><button class='compact' onclick=location.href='betting.php'>Betting Game</button></td></tr>";
                 // echo "<tr><td><button class='compact' onclick=location.href='betting.php'>TV</button></td></tr>";
                 // echo "<tr><td><button class='compact' onclick=location.href='videogame.php'>Video Games</button></td></tr>";
