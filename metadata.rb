a658c2a61f5e5337092aab3d42d45ca6c8c33385
ALeonard9 <aleonard9@hotmail.com>
2015-07-14 07:36:07 -0400

    Delete game enabled

--- metadata.rb
+++ metadata.rb
@@ -5,6 +5,6 @@ license          'All rights reserved'
 description      'Installs/Configures Betting'
 long_description 'Installs/Configures Betting'
 
-version          '2.0.1'
+version          '2.0.2'
 
 depends 'apache2', '2.0.0'
