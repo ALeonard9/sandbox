0cbe09a6c9b37e414da26c4ce6da41460efcf715
ALeonard9 <aleonard9@hotmail.com>
2015-12-01 19:25:12 -0500

    Reskin of betting app

--- files/default/app/bet/betting.php
+++ files/default/app/bet/betting.php
@@ -40,15 +40,12 @@ if ($_SESSION['username'])
                 $intro= "Soumya owes Adam";
                 $dbbalance *= -1;
                 }
-        #echo '<pre>', print_r($queryopen), '</pre>';
-        echo "<h1>".$intro.": $".$dbbalance."</h1>";
-        echo "<br><button class='compact' onclick=location.href='quickbet.php?type=fart'>Quick Win S</button>";
-        echo "<button class='compact' onclick=location.href='quickbet.php?type=pick'>Quick Win A</button>";
-        echo "<br><button class='compact' onclick=location.href='newbet.php'>New Bet</button>";
+        echo "<div class='container text-center'><h1>".$intro.": $".$dbbalance."</h1>";
+        echo "<div class='row'><div class='col-md-4'><button type='button' class='btn btn-inverse btn-lg btn-block' onclick=location.href='quickbet.php?type=fart'>Quick Win S</button></div>";
+        echo "<div class='col-md-4'><button type='button' class='btn btn-inverse btn-lg btn-block' onclick=location.href='quickbet.php?type=pick'>Quick Win A</button></div>";
+        echo "<div class='col-md-4'><button type='button' class='btn btn-inverse btn-lg btn-block' onclick=location.href='newbet.php'>New Bet</button></div></div>";
         echo "<br><h3>Open Bets</h3>";
-        echo "<!DOCTYPE html>";
-        echo "<html>";
-        echo "<table border='2'>";
+        echo "<table class='table table-hover table-striped'>";
         echo "<tr><td>Description</td><td>Amount</td><td>Last Update</td></tr>";
 
                 foreach($queryopen as $item){
@@ -56,8 +53,8 @@ if ($_SESSION['username'])
                 }
         echo "</table>";
 
-        echo "<br><h3>History</h3>";
-        echo "<table border='2'>";
+        echo "<h3>History</h3>";
+        echo "<table class='table table-hover table-striped'>";
         echo "<tr><td>Description</td><td>Amount</td><td>Status</td><td>Winner</td><td>Last Update</td></tr>";
                 foreach($queryall as $item){
                         echo "<tr><td><a href='betdetails.php?betID=".($item['betID']."'>".$item['betDescription']."</a></td><td>$".abs($item['betAmount'])."</td><td>".$item['betStatus']."</td><td>".$item['betWinner']."</td><td>".substr($item['betDate'],5, 5)."</td></tr>");
@@ -70,5 +67,5 @@ else
         die("You must login");
 
 include('../footer.php');
-echo "</div></body></html>";
+echo "</table></div></div></body></html>";
 ?>
