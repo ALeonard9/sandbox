ba99348516be01886d5b7fb8b3920494b27a2e1a
ALeonard9 <aleonard9@hotmail.com>
2016-01-05 07:18:00 -0500

    Corrected oauth url

--- attributes/default.rb
+++ attributes/default.rb
@@ -2,4 +2,4 @@ default['apache']['docroot_dir'] = '/var/www/cgi-bin'
 default['apache']['default_site_enabled'] = true
 
 default['projectorion']['servername'] = 'aleonard.us'
-default['projectorion']['OAuth_URL'] = "http://#{node['projectorion']['servername']}/users/oauth.php"
+default['projectorion']['OAuth_URL'] = "http://#{node['projectorion']['servername']}/users/signin.php"
--- metadata.rb
+++ metadata.rb
@@ -5,6 +5,6 @@ license 'All rights reserved'
 description 'Installs/Configures Betting'
 long_description 'Installs/Configures Betting'
 
-version '2.1.0'
+version '2.1.1'
 
 depends 'apache2'
